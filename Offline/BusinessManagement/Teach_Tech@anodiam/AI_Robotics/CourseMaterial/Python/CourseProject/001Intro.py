419ce16be400f97e83a3f412db761e0de03b2c44
Anodiam <66468713+anodiamadm@users.noreply.github.com>
2023-07-05 18:51:07 +0530

    Check In

---
+++ Offline/BusinessManagement/Teach_Tech@anodiam/AI_Robotics/CourseMaterial/Python/CourseProject/001Intro.py
@@ -0,0 +1,13 @@
+print('Hello School')
+
+x = 2
+y = 3
+print("Answer is: ", x + y)
+print("Hi there! " + "My answer is: ", x + y)
+
+print(5 / 2)
+
+print("Rupa\'s “Laptop”\'s CPU\n\tWorks very fast!")
+print("My RAW String is: ", r"c:\docs\navin")
+
+print(10 * 'school')

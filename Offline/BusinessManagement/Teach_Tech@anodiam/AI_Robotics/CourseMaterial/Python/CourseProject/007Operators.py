1d7e5b22d82b36a52f3c13b02f9f335695dfe6e9
Anodiam <66468713+anodiamadm@users.noreply.github.com>
2023-07-11 13:22:33 +0530

    chk in

---
+++ Offline/BusinessManagement/Teach_Tech@anodiam/AI_Robotics/CourseMaterial/Python/CourseProject/007Operators.py
@@ -0,0 +1,23 @@
+a = 5
+b = 6
+print("sum of a and b:", a + b)
+x = 7
+x += 5
+print("sum of x and 5:", x)
+x *= 3
+print("x multiplied by 3:", x)
+
+e, f = 4, 9
+print(e)
+print(f)
+
+n = 90
+n = -n
+print(n)
+
+print("a greater than b:", a > b)
+print("n less than b:", n < b)
+print("a = b:", a == b)
+print("a greater or equal to b:", a >= b)
+print("a lesser or equal to b:", a <= b)
+

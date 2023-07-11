9bf2cbb97d2daead3407052e3838a219477a679d
Anodiam <66468713+anodiamadm@users.noreply.github.com>
2023-07-11 15:29:06 +0530

    commit

--- Offline/BusinessManagement/Teach_Tech@anodiam/AI_Robotics/CourseMaterial/Python/CourseProject/007Operators.py
+++ Offline/BusinessManagement/Teach_Tech@anodiam/AI_Robotics/CourseMaterial/Python/CourseProject/007Operators.py
@@ -1,3 +1,5 @@
+# ARITHMETIC OPERATORS
+
 a = 5
 b = 6
 print("sum of a and b:", a + b)
@@ -7,17 +9,33 @@ print("sum of x and 5:", x)
 x *= 3
 print("x multiplied by 3:", x)
 
+# ASSIGNMENT OPERATORS
+
 e, f = 4, 9
 print(e)
 print(f)
 
+# UNARY OPERATOR
+
 n = 90
 n = -n
 print(n)
+
+# RELATIONAL OPERATORS
 
 print("a greater than b:", a > b)
 print("n less than b:", n < b)
 print("a = b:", a == b)
 print("a greater or equal to b:", a >= b)
 print("a lesser or equal to b:", a <= b)
+print("a not equal to b:", a != b)
 
+# LOGICAL OPERATORS
+
+a, b = 5, 6
+print("a greater than 8 AND b greater than 7:", a > 8 and b > 7)
+print("a greater than 8 AND b lesser than 7:", a > 8 and b < 7)
+print("a greater than 8 OR b lesser than 7:", a > 8 or b < 7)
+x = 'true'
+x = not x
+print("reverse of x:", x)
